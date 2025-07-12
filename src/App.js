57f3a2cc726d15547763c48c241479083da4958c
unknown <muktinathrajbanshi786@gmail.com>
2025-07-12 15:07:57 +0545

    Just adding design for the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "./App.css";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to the redux page</h1>
+      <div className="conatiner">
+        <h1>Increment/Decrement counter</h1>
+        <h4>using React and Redux</h4>
+        
+        <div className="quantity">
+          <button className="quantity__minus" title="Decrement"><span>-</span></button>
+          <input type="text" name="quantity" className="quantity__input" value="0" />
+          <button className="quantity__plus" title="Increment"><span>+</span></button>
+        </div>
+      </div>
     </>
   );
 };

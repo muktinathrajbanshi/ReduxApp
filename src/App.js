93e422fbcd2f140d228ce7f52817a5511ebe3c1c
unknown <muktinathrajbanshi786@gmail.com>
2025-07-12 17:40:06 +0545

    finally design is completed for my project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <div className="conatiner">
+      <div className="container">
         <h1>Increment/Decrement counter</h1>
         <h4>using React and Redux</h4>
         
         <div className="quantity">
           <button className="quantity__minus" title="Decrement"><span>-</span></button>
-          <input type="text" name="quantity" className="quantity__input" value="0" />
+          <input type="text" name="quantity" className="quantity__input" value="60" />
           <button className="quantity__plus" title="Increment"><span>+</span></button>
         </div>
       </div>

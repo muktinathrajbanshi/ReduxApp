3adb5e0b263f8449c41c7f92788ac0c35d7715de
unknown <muktinathrajbanshi786@gmail.com>
2025-07-14 13:46:05 +0545

    My extra redux app started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { useSelector, useDispatch } from 'react-redux';
 import {incNumber, decNumber} from "./actions/index";
 
@@ -16,6 +17,17 @@ const App = () => {
           <button type="button" className="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber(5))}><span>-</span></button>
           <input type="text" name="quantity" className="quantity__input" value={myState} readOnly/>
           <button type="button" className="quantity__plus" title="Increment" onClick={ () => dispatch(incNumber(5))}><span>+</span></button>
+        </div>
+      </div>
+
+      <div className="container my-5">
+        <h1>Multiplication/Division counter</h1>
+        <h4>using React and Redux</h4>
+        
+        <div className="quantity">
+          <button type="button" className="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber(5))}><span>/</span></button>
+          <input type="text" name="quantity" className="quantity__input" value={myState} readOnly/>
+          <button type="button" className="quantity__plus" title="Increment" onClick={ () => dispatch(incNumber(5))}><span>*</span></button>
         </div>
       </div>
     </>
